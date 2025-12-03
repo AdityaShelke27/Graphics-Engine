9413be9c3cafa1d46b7cd6966fee59a11af9e627
Aditya Shelke <ShelkeAditya325@gmail.com>
2025-12-03 21:49:15 +0530

    Started with projection

--- src/main.cpp
+++ src/main.cpp
@@ -189,14 +189,9 @@ int main()
 
     //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-    glm::mat4 trans(1);
+    glm::mat4 modal(1);
     
-    trans = glm::scale(trans, glm::vec3(0.5f, 0.5f, 0.5f));
-
-    unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
-    glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
-
-    
+    modal = glm::rotate(modal, glm::radians(-45.0f), glm::vec3(1.0f, 0.0f, 0.0f));
 
     while (!glfwWindowShouldClose(window))
     {
@@ -208,12 +203,6 @@ int main()
         ourShader.use();
 
         ourShader.setFloat("alphaValue", alphaValue);
-
-        float time = (float)glfwGetTime();
-        trans = glm::mat4(1);
-        trans = glm::translate(trans, glm::vec3(-cos(time) / 2.0f, sin(time) / 2.0f, 0.0f));
-        trans = glm::rotate(trans, glm::radians(time * 5), glm::vec3(0.0f, 0.0f, 1.0f));
-        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
 
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, texture);
